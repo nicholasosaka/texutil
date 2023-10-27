ee378ed9c454fcb3be146c7a1a33fe89f4059a46
Nicholas Osaka <nicholas.osaka@gmail.com>
2023-10-27 00:02:27 -0400

    Add temp file creation

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,9 +1,20 @@
 from click.testing import CliRunner
 from texutil.scripts.txu import clean
-import pytest
+import pytest, os, pathlib
 
-def test_clean_requires_argument():
-    runner = CliRunner()
-    result = runner.invoke(clean)
-    assert result.exit_code == 2
 
+@pytest.fixture(autouse=True)
+def create_temp_files():
+    sample_file_ext = [
+        'aux', 'fdb_latexmk', 'fls', 'log', 'out', 'pdf', 'dvi', 'synctex.gz', 'tex', 'txt', 'py'
+    ]
+
+    pathlib.Path('tmp/').mkdir()
+    for ext in sample_file_ext:
+        pathlib.Path(f'tmp/sample_file.{ext}').touch()
+
+class TestClean:
+    def test_clean_requires_argument(self):
+        runner = CliRunner()
+        result = runner.invoke(clean)
+        assert result.exit_code == 2
